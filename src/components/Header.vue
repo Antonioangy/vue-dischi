fe2d927e91360d4377e7dea7af4db569932896dd
Antonioangy <dumitruangy26@gmail.com>
2021-11-29 15:29:53 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="banner">
+    <div id="logo">
+      <img src="@/assets/img/logo2.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#banner {
+  height: 80px;
+  background-color: #2e3a46;
+  display: flex;
+}
+#logo{
+  color: white;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+}
+img{
+  height: 50px;
+}
+</style>
